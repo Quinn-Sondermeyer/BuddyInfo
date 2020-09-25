3dea70c1baf5a4b366a830667b98fcda709c3d4e
Quinn-Sonermeyer <71237644+Quinn-Sonermeyer@users.noreply.github.com>
2020-09-25 10:51:27 -0400

    Made changes to Added Functions and main method calls

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Address Book about buddy info");
+    ArrayList<BuddyInfo> book;
+
+    public AddressBook() {
+        book = new ArrayList<>();
     }
 
+    public static void main(String[] args) {
+        System.out.println("Address Book about buddy info");
+        BuddyInfo bud_1 = new BuddyInfo("jet", "420-420-9999", "4 ontario st");
+        AddressBook addressbook = new AddressBook();
+        addressbook.addBuddy(bud_1);
+        addressbook.removeBuddy(bud_1);
+    }
+
+    /**
+     * Adds a person to the List of Buddys
+     */
+    public void addBuddy(BuddyInfo bud) {
+        book.add(bud);
+    }
+    /**
+     * removes a person to the List of Buddys
+     */
+    public void removeBuddy(BuddyInfo bud) {
+        book.remove(bud);
+    }
 }
